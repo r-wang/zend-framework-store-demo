4ca988f1aa9206923cb7a8be093124f5a1cac7b0
Ruiqi Wang <wangruiqicn@gmail.com>
2013-06-19 23:01:22 +0800

    Minor Fix

--- application/models/utils/Validation.php
+++ application/models/utils/Validation.php
@@ -1,75 +1,149 @@
 <?php
+
 /**
+
  * This class encapsulates string validation logics
+
  * 
+
  */
+
 class Validation {
+
 	// array to store validaton rules
+
 	protected $_valid = array ();
+
 	
+
 	// array to store errors out of validation
+
 	protected $error = array ();
+
 	
+
 	/**
+
 	 * Sets validation rules.
+
 	 *
+
 	 * @param $rules Array       	
+
 	 */
+
 	public function setRules($rules) {
+
 		$this->_valid = $rules;
+
 	}
+
 	
+
 	public function getErrors() {
+
 		return $this->error;
+
 	}
+
 	
+
 	/**
+
 	 * Validates an array of values against an array of rules and
+
 	 * save validation results in an array.
+
 	 *
+
 	 * @param $data string       	
+
 	 * @param $rules string       	
+
 	 */
-	public function _validate($data, $rules) {
+
+	public function _validate($data) {
+
 		for($i = 0; $i < count ( $data ); $i ++) {
-			if ($this->check ( $data [$i], $rules [$i] )) {
-				$this->error [] = $rules [$i];
+
+			if ($this->check ( $data [$i], $this->rules [$i] )) {
+
+				$this->error [] = $this->rules [$i];
+
 			}
+
 		}
+
 	}
+
 	
+
 	/**
+
 	 *
+
 	 * @param $value string
+
 	 *       	 value to be validated
+
 	 * @param $rule string
+
 	 *       	 rule against which value is to be validated
+
 	 *       	
+
 	 * @return boolean true represents that errors exist and false represents
+
 	 *         that
+
 	 *         no error exists
+
 	 */
+
 	protected function check($value, $rule) {
+
 		switch ($rule) {
+
 			case "require" :
+
 				return ! empty ( $value );
+
 				break;
+
 			case "email" :
+
 				return ! $this->isEmail ( $value );
+
 				break;
+
 		}
+
 		return false;
+
 	}
+
 	
+
 	/**
+
 	 * Checks if a string is a valid email address
+
 	 *
+
 	 * @param $value string       	
+
 	 * @return boolean
+
 	 */
+
 	protected function isEmail($value) {
+
 		return preg_match ( "/^[a-zA-Z0-9_]+@[a-zA-Z]+(.[a-zA-Z]+)+$/i", $value );
+
 	}
+
 }
+
+
 
 ?>
--- public/js/validateRegistrationForm.js
+++ public/js/validateRegistrationForm.js
@@ -1,76 +1,145 @@
 $(document).ready(function() {
 
+
+
 	$(".submit").click(function() {
+
 		var errors = new Array();
+
 		var name = $(":input[name*=name]").val();
+
 		if (name.length == 0) {
+
 			errors['name'] = "字段不能为空";
+
 		} else {
+
 			errors['name'] = null
+
 		}
+
+
 
 		var email_reg = /^[a-zA-Z0-9_]+@[a-zA-Z]+(\.[a-zA-Z]+)+$/gi;
+
 		var email = $(":input[name*=email]").val();
+
 		if (email.length == 0) {
+
 			errors['email'] = "字段不能为空";
+
 		} else if (!email_reg.test(email)) {
+
 			errors['email'] = "邮箱格式错误";
+
 		} else {
+
 			errors['email'] = null;
+
 		}
+
 		var telephone = $(":input[name*=telephone]").val();
+
 		if (telephone.length == 0) {
+
 			errors['telephone'] = "字段不能为空";
+
 		} else {
+
 			errors['telephone'] = null;
+
 		}
+
 		var address = $(":input[name*=address]").val();
+
 		if (address.length == 0) {
+
 			errors['address'] = "字段不能为空";
+
 		} else {
+
 			errors['address'] = null;
+
 		}
+
 		var password = $(":input[name*=password]").val();
+
 		if (password.length == 0) {
+
 			errors['password'] = "字段不能为空";
+
 		} else {
+
 			errors['password'] = null;
+
 		}
+
 		var verifyPassword = $(":input[name*=verifyPassword]").val();
+
 		if (verifyPassword.length == 0) {
+
 			errors['verifyPassword'] = "字段不能为空";
+
 		} else if (password !== verifyPassword) {
+
 			errors['verifyPassword'] = "请再输入确认密码";
+
 		} else {
+
 			errors['verifyPassword'] = null;
+
 		}
+
 		var isVerified = true;
+
 		for ( var i in errors) {
 
+
+
 			$("#Customer_" + i + "_em_").empty();
+
 			$("#Customer_" + i + "_em_").append(errors[i]);
+
 			$("#Customer_" + i + "_em_").show();
+
 			if (errors[i] != null) {
+
 				isVerified = false;
+
 			}
 
+
+
 		}
+
 		$.post("/customer/checkdata", {
+
 			"email" : email
+
 		}, function(data) {
 
 			if (data !== "ok") {
 				isVerified = false;
-				$("#Customer_email_em_").empty();
-				$("#Customer_email_em_").append(data);
-				$("#Customer_email_em_").show();
-			}
 
-		});
+				$("#Customer_email_em_").empty();
+
+				$("#Customer_email_em_").append(data);
+
+				$("#Customer_email_em_").show();
+				
+
+			} 
 		if (isVerified) {
+
 			$("#register-form").submit();
+
 		}
 
+		});
+
+
 	});
+
+
 
 });
